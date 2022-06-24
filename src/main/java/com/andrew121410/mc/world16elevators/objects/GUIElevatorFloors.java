084fe3cfc9387c76d4a2049b0da6b9ee248ff1dc
Andrew121410 <andrewcherring@gmail.com>
2022-06-24 18:47:54 -0400

    Bump World1-6Utils

--- src/main/java/com/andrew121410/mc/world16elevators/objects/GUIElevatorFloors.java
+++ src/main/java/com/andrew121410/mc/world16elevators/objects/GUIElevatorFloors.java
@@ -1,7 +1,7 @@
 package com.andrew121410.mc.world16elevators.objects;
 
 import com.andrew121410.mc.world16elevators.World16Elevators;
-import com.andrew121410.mc.world16utils.gui.AdvanceGUIWindow;
+import com.andrew121410.mc.world16utils.gui.GUIWindow;
 import com.andrew121410.mc.world16utils.gui.buttons.GUIButton;
 import com.andrew121410.mc.world16utils.gui.buttons.defaults.ClickEventButton;
 import com.andrew121410.mc.world16utils.utils.InventoryUtils;
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
-public class GUIElevatorFloors extends AdvanceGUIWindow {
+public class GUIElevatorFloors extends GUIWindow {
 
     private World16Elevators plugin;
     private ElevatorObject elevatorObject;
