7f67f360f774e1d860bcf09e87a1b998b063e3e7
Andrew121410 <andrewcherring@gmail.com>
2023-02-27 01:11:59 -0500

    Switch to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.andrew121410.mc"
+version = "1.0-SNAPSHOT"
+description = "World1-6Elevators"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+
+    maven {
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+
+    maven {
+        url = uri("https://repo.opencollab.dev/maven-releases/")
+    }
+
+    maven {
+        url = uri("https://repo.opencollab.dev/maven-snapshots/")
+    }
+
+    maven {
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("com.github.World1-6.World1-6Utils:World1-6Utils-Plugin:eb356eb1")
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    compileOnly("org.geysermc.floodgate:api:2.2.0-SNAPSHOT")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
